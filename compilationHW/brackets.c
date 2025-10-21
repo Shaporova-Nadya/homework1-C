696aeb4405d37760a4ae2cac27dbd7db8890158a
Nadya Shaporova <saporovanada@gmail.com>
2025-10-21 23:42:49 +0300

    Add fixed brackets task

--- compilationHW/brackets.c
+++ compilationHW/brackets.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
-#include <string.h>
+
+int isBalanced(const char* str) 
+{
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '(') {
+            count++;
+        } else if (str[i] == ')') {
+            count--;
+        }
+        if (count < 0) {
+            return 0; 
+        }
+    }
+    return (count == 0); 
+}
 
 int main(void) 
 {
-  char str[] = "((a + b) * (c + d)";
-  int count = 0;
-  int i;
-  for (i = 0; str[i] != '\0'; i++) 
-  {
-    if (str[i] == '(') 
-    {
-      count++;
-    } 
-    else if (str[i] == ')') 
-    {
-      count--;
+    char str[] = "((a + b) * (c + d)"; 
+    
+    if (isBalanced(str)) {
+        printf("Скобки сбалансированны\n");
+    } else {
+        printf("Скобки не сбалансированны\n");
     }
-  }
-  if (count == 0) 
-  {
-    printf("Скобки сбалансированны\n");
-  } 
-  else 
-  {
-    printf("Скобки не сбалансированны\n");
-  }
-  return 0;
+    
+    return 0;
 }
