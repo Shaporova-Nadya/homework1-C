ca64a629d8f0f2b9fff108b8eecd75cd13a5ecd7
Nadya Shaporova <saporovanada@gmail.com>
2025-11-10 21:21:51 +0300

    Changed stack.h

--- stack/stack.h
+++ stack/stack.h
@@ -11,39 +11,13 @@ typedef struct Stack {
 }Stack;
 
 //Function that creates stack
-Stack new(void) { 
-    Stack stack = { 
-        .head = NULL 
-    };
-    return stack;
-}
-
+Stack new(void) 
+    
 //Function for checking the stack for emptiness
-int isEmpty(Stack* stack) { 
-    return stack->head == NULL;
-}
+int isEmpty(Stack* stack) 
 
 //Function for pushing elements to stack
-void push(Stack* stack, char value) {
-    Node* node = (Node*)malloc(sizeof(Node));
-    if (node == NULL) { 
-        fprintf(stderr, "Memory allocation failed in push\n");
-        exit(EXIT_FAILURE);
-    }
-    node->value = value;
-    node->next = stack->head;
-    stack->head = node;
-}
+void push(Stack* stack, char value) 
 
 //This function pops elements from stack
-char pop(Stack* stack) {
-    if (isEmpty(stack)) {
-        fprintf(stderr, "Attempted to pop from an empty stack\n");
-        exit(EXIT_FAILURE); 
-    }
-    Node* oldNode = stack->head;
-    char res = oldNode->value;
-    stack->head = oldNode->next;
-    free(oldNode);
-    return res;
-}
+char pop(Stack* stack) 
