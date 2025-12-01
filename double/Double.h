1c58e8677f3d91b7555521a9bbe8f26761e1c0b0
Nadya Shaporova <saporovanada@gmail.com>
2025-12-02 00:02:02 +0300

    Add Double.h

---
+++ double/Double.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef union Number {
+    double value;
+    unsigned char binaryForm[8]; 
+} Number;
+
+bool getBit(unsigned char data[8], int bitIndex);
+
+void printDouble(double value);
+
