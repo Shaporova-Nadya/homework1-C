62b584d64020f6b6dc47a071794381db002d44d9
Nadya Shaporova <saporovanada@gmail.com>
2025-12-10 19:43:40 +0300

    Corrected style

--- src/polynomial.c
+++ src/polynomial.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-int formule (int x)
+
+int
+formule(int x)
 {
-int x1;
-x1 = ((x + 1) * x + 1) * x + 1;
-return x1;
+    int x1;
+    x1 = ((x + 1) * x + 1) * x + 1;
+    return x1;
 }
-int main () 
+
+int
+main(void)
 {
-int num;
-printf("Введите число x:");
-scanf("%d", &num);
-int result = formule(num);
-printf("Значение формулы равно: %d", result);
-return 0;
+    int num;
+
+    printf("Введите число x:");
+    scanf("%d", &num);
+
+    int result = formule(num);
+
+    printf("Значение формулы равно: %d", result);
+    return 0;
 }
