2f01068849baf6307242f70413a33895fcb244be
Nadya Shaporova <saporovanada@gmail.com>
2025-12-10 19:43:11 +0300

    Corrected style

--- src/lucky_tickets.c
+++ src/lucky_tickets.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
-int main(void) 
+int
+main(void)
 {
     int count = 0;
-    int sums[28] = {0}; 
-    
-    for (int i = 0; i <= 9; i++) 
-    {
-        for (int j = 0; j <= 9; j++) 
-        {
-            for (int k = 0; k <= 9; k++) 
-            {
+    int sums[28] = { 0 };
+
+    for (int i = 0; i <= 9; i++) {
+        for (int j = 0; j <= 9; j++) {
+            for (int k = 0; k <= 9; k++) {
                 sums[i + j + k]++;
             }
         }
     }
 
-    
-    for (int i = 0; i <= 27; i++) 
-    {
+    for (int i = 0; i <= 27; i++) {
         count += sums[i] * sums[i];
     }
 
