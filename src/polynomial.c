351d4afb6741960c2252aaa90c41e9854ee00684
Nadya Shaporova <saporovanada@gmail.com>
2025-12-10 19:47:05 +0300

    Corrected style

--- src/polynomial.c
+++ src/polynomial.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-int
-formule(int x)
+int formule(int x)
 {
     int x1;
     x1 = ((x + 1) * x + 1) * x + 1;
     return x1;
 }
 
-int
-main(void)
+int main(void)
 {
     int num;
 
