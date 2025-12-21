53820c56a2a541616863e43df9bcca4b42cddb14
Nadya Shaporova <saporovanada@gmail.com>
2025-12-21 15:01:07 +0300

    Fixed file

--- dataHW/main.c
+++ dataHW/main.c
@@ -3,25 +3,20 @@
 #include <stdlib.h>
 #include "Lib.h"
 
-int main(void)
-{
-    int num1, num2
-    printf("Введите первое число:");
-    scanf("%d", num1);
-    printf("Введите второе число:");
-    scanf("%d", num2);
+int main(void) {
+    int num1, num2; 
+    
+    printf("Введите первое число: ");
+    if (scanf("%d", &num1) != 1) return 1; 
+    
+    printf("Введите второе число: ");
+    if (scanf("%d", &num2) != 1) return 1; 
 
-
-    printf("\n Число1 (%d)=", num1);
-    binary(num1);
-    printf("\n Число2 (%d)=", num2);
-    binary(num2);
-    printf("\n");
-
+    printf("\n--- Результат сложения ---\n");
     BinarySum(num1, num2);
 
     char FinalSum = (char)((unsigned char)num1 + (unsigned char)num2);
-    printf("Сумма в десятичном виде: %d\n", FinalSum);
+    printf("\nСумма в десятичном виде: %d\n", FinalSum);
 
     return 0;
 }
