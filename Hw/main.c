a85245646d2aedbcf0a4d0ca005771887c63fff0
Nadya Shaporova <saporovanada@gmail.com>
2025-12-21 16:04:30 +0300

    Fixed task and added cmake

--- Hw/main.c
+++ Hw/main.c
@@ -1,24 +1,35 @@
-#include "sorting.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() 
-{
-  const int MaxNumber = 100;
-  int numbers[MaxNumber];
-  int count = 0;
-  int num;
-  while (count < MaxNumber && scanf("%d", &num) == 1) 
-  {
-    numbers[count++] = num;
-  }
-  qsort(numbers, count, sizeof(int), CompareFunc);
+extern int asmSort(int *array, int n);
 
-  for (int i = 0; i < count; i++) 
-  {
-    printf("%d%s", numbers[i], (i == count - 1) ? "" : " ");
-    
-  }
-  printf("\n");
-    return 0;
-  }
+int main(void)
+{
+    int numbers[100];
+    int count = 0;
+    int val;
+    char ch;
+
+    while (count < 100) {
+        if (scanf("%d", &val) == 1) {
+            numbers[count++] = val;
+        }
+
+        ch = getchar();
+        if (ch == '\n' || ch == EOF) {
+            break;
+        }
+    }
+
+    if (count == 0) {
+        return 0;
+    }
+
+    int movedElements = asmSort(numbers, count);
+
+    for (int i = 0; i < count; i++) {
+        printf("%d%c", numbers[i], (i == count - 1) ? '\n' : ' ');
+    }
+
+    return movedElements;
+}
--- Hw/sorting.s
+++ Hw/sorting.s
@@ -1,44 +1,58 @@
-.file	"sorting.c"
+	.file	"sorting.c"
 	.text
-	.globl	CompareFunc
-	.type	CompareFunc, @function
-CompareFunc:
-.LFB6:
+	.p2align 4
+	.globl	asmSort
+	.type	asmSort, @function
+asmSort:
+.LFB39:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movq	%rdi, -24(%rbp)
-	movq	%rsi, -32(%rbp)
-	movq	-24(%rbp), %rax
-	movl	(%rax), %eax
-	movl	%eax, -8(%rbp)
-	movq	-32(%rbp), %rax
-	movl	(%rax), %eax
-	movl	%eax, -4(%rbp)
-	movl	-8(%rbp), %eax
-	cmpl	-4(%rbp), %eax
-	jge	.L2
-	movl	$-1, %eax
-	jmp	.L3
-.L2:
-	movl	-8(%rbp), %eax
-	cmpl	-4(%rbp), %eax
-	jle	.L4
-	movl	$1, %eax
-	jmp	.L3
-.L4:
-	movl	$0, %eax
+	cmpl	$1, %esi
+	jle	.L7
+	leal	-1(%rsi), %r11d
+	xorl	%r9d, %r9d
+	xorl	%r10d, %r10d
+	.p2align 4,,10
+	.p2align 3
+.L6:
+	movl	4(%rdi,%r9,4), %esi
+	movq	%r9, %rax
+	xorl	%r8d, %r8d
+	.p2align 4,,10
+	.p2align 3
 .L3:
-	popq	%rbp
-	.cfi_def_cfa 7, 8
+	movl	(%rdi,%rax,4), %edx
+	movl	%eax, %ecx
+	cmpl	%esi, %edx
+	jle	.L11
+	movl	%edx, 4(%rdi,%rax,4)
+	subq	$1, %rax
+	movl	$1, %r8d
+	cmpl	$-1, %eax
+	jne	.L3
+	movq	%rdi, %rax
+.L4:
+	addq	$1, %r9
+	movl	%esi, (%rax)
+	addl	%r8d, %r10d
+	cmpq	%r9, %r11
+	jne	.L6
+	movl	%r10d, %eax
+	ret
+	.p2align 4,,10
+	.p2align 3
+.L11:
+	addl	$1, %ecx
+	movslq	%ecx, %rcx
+	leaq	(%rdi,%rcx,4), %rax
+	jmp	.L4
+.L7:
+	xorl	%r10d, %r10d
+	movl	%r10d, %eax
 	ret
 	.cfi_endproc
-.LFE6:
-	.size	CompareFunc, .-CompareFunc
+.LFE39:
+	.size	asmSort, .-asmSort
 	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04.2) 11.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
