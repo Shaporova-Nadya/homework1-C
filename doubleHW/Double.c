50ba7b159aae4d3f67eb78dccbd5500428df451c
Nadya Shaporova <saporovanada@gmail.com>
2025-12-21 14:06:36 +0300

    Fixed file

--- doubleHW/Double.c
+++ doubleHW/Double.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <math.h> 
 #include "Double.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 typedef union Number {
     double value;
-    unsigned char binaryForm[8]; 
+    unsigned char binaryForm[8];
 } Number;
 
-bool getBit(unsigned char data[8], int bitIndex) {
+bool getBit(unsigned char data[8], int bitIndex)
+{
     int byte = bitIndex / 8;
     int bitInByte = bitIndex % 8;
-    
-    unsigned char mask = bitInByte >> 1;
-    
+    unsigned char mask = 1 << bitInByte;
     return (data[byte] & mask) != 0;
 }
 
-void printDouble(double value) {
+void printDouble(double value)
+{
     if (value == 0.0) {
         printf("Result: +0.0*2^0\n");
         return;
@@ -26,29 +26,35 @@ void printDouble(double value) {
     Number converter;
     converter.value = value;
 
-    bool signBit = get_bit(converter.binaryForm, 63);
+    bool signBit = getBit(converter.binaryForm, 63);
     char signChar = signBit ? '-' : '+';
 
     int exponentBits = 0;
     for (int i = 0; i < 11; i++) {
-        if (get_bit(converter.binaryForm, 52 + i)) {
+        if (getBit(converter.binaryForm, 52 + i)) {
             exponentBits |= (1 << i);
         }
     }
 
     double mantissa = 0.0;
-    double pow = 2.0; 
-
+    double divisor = 2.0;
     for (int i = 0; i < 52; i++) {
-        if (get_bit(converter.binaryForm, 51 - i)) {
-            mantissa += (1.0 / pow);
+        if (getBit(converter.binaryForm, 51 - i)) {
+            mantissa += (1.0 / divisor);
         }
-        pow *= 2.0;
+        divisor *= 2.0;
     }
 
     int p = exponentBits - 1023;
     double m = 1.0 + mantissa;
 
+    if (exponentBits == 0) {
+        p = -1022;
+        m = mantissa;
+    } else if (exponentBits == 2047) {
+        printf("Result: %cSpecial Value\n", signChar);
+        return;
+    }
 
     printf("Result: %c%.15g*2^%d\n", signChar, m, p);
 }
