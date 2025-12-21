5d08d26ac61bb464959131669515c0947d4ef968
Nadya Shaporova <saporovanada@gmail.com>
2025-12-21 15:00:37 +0300

    Fixed file

--- dataHW/Lib.c
+++ dataHW/Lib.c
@@ -3,30 +3,26 @@
 #include <stdlib.h>
 #include "Lib.h"
 
-void binary(int x)
-{
+void binary(int x) {
     unsigned char n = (unsigned char)x;
-    int bit = 0b10000000;
-for (int j = 0; j < 8; j++){
-    printf("%c"(x & bit)?"1":"0", "\n");
-    bit = bit >> 1;
-}
+    unsigned char bit = 0x80; 
+
+    for (int j = 0; j < 8; j++) {
+        printf("%s", (n & bit) ? "1" : "0");
+        bit = bit >> 1;
+    }
 }
 
-void BinarySum(int a, int b)
-{
+void BinarySum(int a, int b) {
     unsigned char numA = (unsigned char)a;
     unsigned char numB = (unsigned char)b;
     unsigned char sum = numA + numB;
 
-    binary(a);
-    printf("%d\n", a);
-    printf("+");
-    binary(b);
-    printf("%d\n", b);
-    printf("=");
-    printf("\n");
-    binary(sum);
-    printf("%d\n", sum);
+    binary(numA); printf(" (%d)\n", numA);
+    printf("+\n");
+    binary(numB); printf(" (%d)\n", numB);
+    printf("========\n");
+    binary(sum);  printf(" (%d)\n", sum);
 }
+
 
